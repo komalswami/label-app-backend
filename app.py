7c57cfecd6ed780b0f58f26e8fc4b04ed5fa180e
komalswami <komalsswami@gmail.com>
2023-08-21 17:34:12 +0530

    updated code for generting zpl code

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 import sqlite3
 from flask_cors import CORS
-
+import numpy as np
 
 app = Flask(__name__)
 CORS(app) 
@@ -12,40 +12,55 @@ def generate_zpl():
     print("hello world!")
     data = request.get_json()
 
-    # start = "^XA"
-    # end = "^XZ"
-    # label_pos = "^FT"
-    # label_text_tag = "^FD" 
+    print("hello world!",data)
+    start = "^XA"
+    end = "^XZ"
+    label_pos = "^FT"
+    label_text_tag = "^FD" 
 
-    # change_font = "^CF0,30"
-    # start_of_field ="^FO"
-    # end_of_field = "^FS"
-    # input_text = ['product_code','product_name','customer_code','customer_name']
-    # input_label = ['product_code','product_name','customer_code','customer_name']
+    change_font = "^CF0,30"
+    start_of_field ="^FO"
+    end_of_field = "^FS"
+    input_text = ['product_code','product_name','customer_code','customer_name']
+    input_label = ['product_code','product_name','customer_code','customer_name']
+    
+    # input_text = data['label_text']
+    # input_label = data['label_values']
 
-    # input_field = "product_name"
 
-    # x_start_label = 120 
-    # y_start_label = 90
+    input_field = "product_name"
 
-    # x_start_value = 120 
-    # y_start_value = 90
+    x_start_label = 120 
+    y_start_label = 90
+
+    x_start_value = 370
+    y_start_value = 90
 
     
-    # y_diff = 30
+    y_diff_label = 30
+    y_diff_value = 30
 
+    code = []
+    code.append(start)
+    code.append(change_font)
+    
+    for num in range(1, len(input_text)):
+        print("_____________________________",input_text[num])
+        print("******************************",input_label[num])
+        
+        y_start_label = y_start_label+y_diff_label
+        y_start_value = y_start_value + y_diff_value
 
-    # for num in range(1, 6):
-    #     print("_____________________________",input_text[num])
-    #     print("******************************",input_label[num])
+        code.append(start_of_field +str(x_start_label)+","+str(y_start_label)+label_text_tag+input_text[num]+end_of_field +start_of_field +str(x_start_value)+","+str(y_start_value)+label_text_tag+input_label[num]+end_of_field)  
+    
+    code.append(end)
+    #total = start + change_font + code +end
 
-    # #code = start_of_field +str(x)+","+str(y)+label_text_tag+input_text[i]+end_of_field +start_of_field +str(x)+","+str(y)+label_text_tag+input_label[i]+end_of_field  
-    # total = start + change_font + code +end
+    total = np.array(code)
+    #print(start+label_pos+label_text+input_field+end)
+    print(total)
 
-
-    # #print(start+label_pos+label_text+input_field+end)
-    # print(total)
-    return{"new":9,"success":True }
+    return{"code":code,"success":True }
 
 
 @app.route('/data', methods=['POST'])
